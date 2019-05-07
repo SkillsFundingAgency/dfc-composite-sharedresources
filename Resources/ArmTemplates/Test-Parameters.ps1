3506c68e5d3d74233c57e4f0cd65893e1cc437be
NickGraham101 <nick.graham101@googlemail.com>
2019-05-07 16:25:01 +0100

    Initial version

---
+++ Resources/ArmTemplates/Test-Parameters.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param(
+    [switch]$DeployToOwnTenant
+)
+
+$Location = "West Europe"
+
+$DeploymentParameters = @{
+    TemplateParameterFile = ".\Resources\ArmTemplates\test-parameters.json"
+    TemplateFile          = ".\Resources\ArmTemplates\template.json"
+}
+
+if($DeployToOwnTenant.IsPresent) {
+
+    $IsLoggedIn = (Get-AzureRMContext -ErrorAction SilentlyContinue).Account
+    if (!$IsLoggedIn) {
+        Login-AzureRmAccount
+    }
+    elseif ($($IsLoggedIn.Id.split("@")[1] -eq "citizenazuresfabisgov.onmicrosoft.com") -or $($IsLoggedIn.Id.split("@")[1] -eq "fcsazuresfabisgov.onmicrosoft.com")) {
+        throw "Logged in to SFA tenant.  Login to your personal tenant."
+    }
+    
+    $TemplateParamsObject = Get-Content $DeploymentParameters['TemplateParameterFile'] | ConvertFrom-Json
+
+    $ResourceGroupName = $TemplateParamsObject.parameters.ApimResourceGroup.value
+    $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+    if (!$ResourceGroup) {
+        Write-Host "- Creating resource group $ResourceGroupName"
+        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+    }
+
+    $ResourceGroupName = "dfc-my-compositeui-rg"
+    $ResourceGroup = Get-AzureRmResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue
+    if (!$ResourceGroup) {
+        Write-Host "- Creating resource group $ResourceGroupName"
+        New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+    }
+    
+    $DeploymentParameters['ResourceGroup'] = $ResourceGroupName
+    $DeploymentParameters['DeploymentDebugLogLevel'] = "All"
+    
+    Write-Host "- Deploying template"
+    New-AzureRmResourceGroupDeployment @DeploymentParameters
+
+} 
+else {
+
+    $ResourceGroupName = "dfc-test-template-rg"
+
+    $DeploymentParameters['ResourceGroup'] = $ResourceGroupName
+    $DeploymentParameters['ApimResourceGroup'] = $ResourceGroupName
+    $DeploymentParameters['Verbose'] = $true
+
+    Write-Host "- Validating template"
+    Test-AzureRmResourceGroupDeployment @DeploymentParameters
+    
+}
+
